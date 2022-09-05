048f8f1ad94dcca5e8f80c5e6810c0dc5cb7523b
mohinoddin <mohinoddin80@gmail.com>
2022-09-05 20:35:13 +0530

    filter

--- client/src/components/propertylist/PropertyList.js
+++ client/src/components/propertylist/PropertyList.js
@@ -8,6 +8,64 @@ const PropertyList = ( {propertydetails} )=>{
 
   const [isOpen,setIsOpen] = useState(false);
   const [isImage,setIsImage]=useState("");
+  const [userData , setUserData]=useState([]);
+  const [isEnabled,setIsEnabled]=useState(false);
+  const [selected,setSelected]=useState(false);
+
+ console.log(userData)
+
+
+
+  const handleChangeFilter = (e)=>{
+    let filterToApply = e.target.value
+   
+ 
+      if(filterToApply==="Residential"){
+     
+       let res= propertydetails.filter((val)=>val.propertyType===filterToApply)
+      console.log(res)
+      setSelected(true)
+      setUserData(res) 
+    } else if(filterToApply==="Commercial"){
+      let res= propertydetails.filter((val)=>val.propertyType===filterToApply)
+      console.log(res)
+      setSelected(true)
+      setUserData(res) 
+    }
+    // if(allData===null){
+    //   setAllData(users)
+    //   if(filterToApply === "Unsold"){
+    //         const result= users.filter((val)=>val.status===filterToApply);
+    //         setUsers(result);
+    //         console.log(result)
+    //     }else{
+    //         const result= users.filter((val)=>val.property_type===filterToApply)
+    //         setUsers(result)
+    //     }
+      
+    // }else if(users.length!==allData.length){
+    //   setUsers(allData)
+    //   if(filterToApply === "Unsold"){
+    //     const result= allData.filter((val)=>val.status===filterToApply);
+    //     setUsers(result);
+    //     }else{
+    //         const result= allData.filter((val)=>val.property_type===filterToApply)
+    //         setUsers(result)
+    //         // console.log(users)
+    //     }
+      
+    // }else{
+    //     if(filterToApply === "Unsold"){
+    //         const result= users.filter((val)=>val.status===filterToApply);
+    //         setUsers(result);
+    //         }else{
+    //             const result= users.filter((val)=>val.property_type===filterToApply)
+    //             setUsers(result)
+    //         }
+      
+
+    // }
+  }
 
     return(
         <>
@@ -20,7 +78,35 @@ const PropertyList = ( {propertydetails} )=>{
         
             <th className="thtext">Image</th>
        
-            <th className="thtext">Property</th>
+            <th className="thtext" onClick={()=>setIsEnabled(true)}>Property
+            <div>
+            <select  style={isEnabled ?{ display:'block'}:{display:'none'}}
+            onChange={handleChangeFilter}
+            defaultValue={"default"}
+            >
+              {/* {propertydataArr.map((propertytypedata,index)=>{
+                console.log(index,{propertytypedata});
+                <option key={index} value='propertytypedata.propertytypedata'>
+                   propertytypedata.propertytypedata
+                </option>
+              })} */}
+
+<option value={"default"} disabled>
+          Choose an option
+        </option>
+       
+              <option key={2} value="Residential">
+                   Residential
+                </option>
+                <option key={3} value="Commercial">
+                Commercial
+                </option>
+
+            </select>
+            </div>
+
+            </th>
+            
         
             <th className="thtext">Contact</th>
      
@@ -36,7 +122,9 @@ const PropertyList = ( {propertydetails} )=>{
         </tr>
         </thead>
                 
-        {propertydetails.map((propertydata) => (
+        {
+        
+       propertydetails.map((propertydata) => (
           <tbody>
           <tr className='tabledata'>
         {/* <td className="tdtext">{propertydata._id.substr(propertydata._id.length - 7)}</td> */}
